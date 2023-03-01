d75e79d10807d96ab5d06e81725b2b3b2854179d
naveen premkumar <njpremk.16@gmail.com>
2023-03-01 15:04:55 -0500

    made the board stoer the game state instead

--- src/index.js
+++ src/index.js
@@ -4,22 +4,16 @@ import './index.css';
 
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    
-    
     
     render() {
       return (
-        <button className="square" onClick={
-            () => {
-                console.log('square clicked');
-            }
+        <button 
+          className="square" 
+          onClick={() => {
+            this.props.onClick()
+          }
         }>
+
           {this.props.value}
         </button>
       );
@@ -27,8 +21,33 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
+    
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        
+      
+      };
+    
+    }
+    
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+    
+    
     renderSquare(i) {
-      return <Square value = {i}/>;
+      return (
+        <Square 
+          value = {this.state.squares[i]}
+          onClick = {() => this.handleClick(i)}
+
+        />
+      );
     }
   
     render() {
